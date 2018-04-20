ba674b95cdf5fa0779ac8fab10d08d5bae5034bf
Eixon <totalunni@hotmail.fr>
2018-04-20 16:46:43 +0200

    UI Menu

---
+++ Assets/Scripts/Options/PlayGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour {
+
+	public void Play() {
+		SceneManager.LoadScene (1, LoadSceneMode.Single); //à changer avec le nom de la scene de jeu
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# HD3D" 
+"# HD3D"
+
+Si une erreur concernant un web player survient, double-cliquez sur l'erreur pour ouvrir le fichier et commentez la ligne du WP.
